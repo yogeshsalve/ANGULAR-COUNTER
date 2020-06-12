47c6e5edbf3d507cdc3b048eb12389dd74ef2435
YOGESH BALKRISHNA SALVE <53407767+yogeshsalve@users.noreply.github.com>
2020-06-13 02:06:42 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+
+
+  count: number = 0;
+
+
+
+  handleIncrease = () => {
+    this.count = this.count + 1;
+    // if (this.count === 10){
+    //   this.count = 10
+    // }
+    
+    // else
+    // this.count = this.count + 1;
+  };
+
+  handleDecrease = () => {
+    this.count = this.count - 1;
+
+    // if (this.count === 0){
+    //   this.count = 0
+    // }
+    
+    // else
+    // this.count = this.count - 1;
+
+  };
+
+  handleReset = () => {
+    this.count = 0;
+  };
 }
